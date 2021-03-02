02e919496cb6c217daaa8ddb2aad866d3282d58a
Tim Flood <tflood9@gmail.com>
2021-03-02 12:57:07 -0500

    commit tree 34afdf7942b18eb523060dc238faa624420db833

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (int) => {
+  return function (value) {
+    return int * value;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, whichDrivers) => {
+  return whichDrivers(drivers);
+};
